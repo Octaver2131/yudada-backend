74afca0353868660d068fb7b9b7f4f6786a7fdc1
Octaver <Octaver2131@outlook.com>
2025-09-10 06:55:31 +0800

    策略 测评类

---
+++ src/main/java/com/yupi/springbootinit/scoring/CustomTestScoringStrategy.java
@@ -0,0 +1,100 @@
+package com.yupi.springbootinit.scoring;
+
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.yupi.springbootinit.model.dto.question.QuestionContentDTO;
+import com.yupi.springbootinit.model.entity.App;
+import com.yupi.springbootinit.model.entity.Question;
+import com.yupi.springbootinit.model.entity.ScoringResult;
+import com.yupi.springbootinit.model.entity.UserAnswer;
+import com.yupi.springbootinit.model.vo.QuestionVO;
+import com.yupi.springbootinit.service.QuestionService;
+import com.yupi.springbootinit.service.ScoringResultService;
+
+import javax.annotation.Resource;
+import java.sql.Wrapper;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 自定义测试评分策略
+ */
+public class CustomTestScoringStrategy implements ScoringStrategy {
+
+    @Resource
+    private QuestionService questionService;
+
+    @Resource
+    private ScoringResultService scoringResultService;
+
+    @Override
+    public UserAnswer doScore(List<String> choices, App app) throws Exception {
+        Long appId = app.getId();
+        // 1. 根据 id 查询到题目和题目结果信息
+        Question question = questionService.getOne(
+                Wrappers.lambdaQuery(Question.class).eq(Question::getAppId, appId)
+        );
+
+        List<ScoringResult> scoringResultList = scoringResultService.list(
+                Wrappers.lambdaQuery(ScoringResult.class).eq(ScoringResult::getAppId, appId)
+        );
+        // 2. 统计用户每个选择对应的属性个数，如 I = 10 个， E = 5 个
+        Map<String, Integer> optionCount = new HashMap<>();
+
+        QuestionVO questionVO = QuestionVO.objToVo(question);
+        List<QuestionContentDTO> questionContent = questionVO.getQuestionContent();
+
+        for (QuestionContentDTO questionContentDTO : questionContent) {
+            // 遍历答案列表
+            for (String answer : choices) {
+                // 遍历题目中的选项
+                for (QuestionContentDTO.Option option : questionContentDTO.getOptions()) {
+                    // 如果答案和选项的 key 匹配
+                    if (option.getKey().equals(answer)) {
+                        // 获取选项 result 的属性
+                        String result = option.getResult();
+
+                        // 如果选项 result 属性不在 optionContent 中，初始化为 0
+                        if (!optionCount.containsKey(result)) {
+                            optionCount.put(result, 0);
+                        }
+                        // 在 optionContent 中增加计数
+                        optionCount.put(result, optionCount.get(result) + 1);
+                    }
+                }
+            }
+        }
+        // 3. 遍历每种评分结果，计算哪个结果的得分更高
+        // 初始化最高分数和最高分数对应的评分结果
+        int maxScore = 0;
+        ScoringResult maxScoringResult = scoringResultList.get(0);
+
+        // 遍历评分结果列表
+        for (ScoringResult scoringResult : scoringResultList) {
+            List<String> resultProp = JSONUtil.toList(scoringResult.getResultProp(), String.class);
+            // 计算当前评分结果的分数
+            int score = resultProp.stream()
+                    .mapToInt(prop -> optionCount.getOrDefault(prop, 0))
+                    .sum();
+
+            // 如果是分数高于当前最高分数，则更新最高分数和最高分数对应的评分结果
+            if (score > maxScore) {
+                maxScore = score;
+                maxScoringResult = scoringResult;
+            }
+        }
+        // 4. 构造返回值，填充答案对象的属性
+        UserAnswer userAnswer = new UserAnswer();
+        userAnswer.setAppId(appId);
+        userAnswer.setAppType(app.getAppType());
+        userAnswer.setScoringStrategy(app.getScoringStrategy());
+        userAnswer.setChoices(JSONUtil.toJsonStr(choices));
+        userAnswer.setResultId(maxScoringResult.getId());
+        userAnswer.setResultName(maxScoringResult.getResultName());
+        userAnswer.setResultDesc(maxScoringResult.getResultDesc());
+        userAnswer.setResultPicture(maxScoringResult.getResultPicture());
+        return userAnswer;
+    }
+}
+
