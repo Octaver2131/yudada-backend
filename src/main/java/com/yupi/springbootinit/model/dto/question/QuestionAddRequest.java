b326f5f8488a23834cf346e1de50d5dd3e199aa7
Octaver <Octaver2131@outlook.com>
2025-09-10 05:20:13 +0800

    List<QuestionContentDTO> 多道题

--- src/main/java/com/yupi/springbootinit/model/dto/question/QuestionAddRequest.java
+++ src/main/java/com/yupi/springbootinit/model/dto/question/QuestionAddRequest.java
@@ -3,6 +3,7 @@ package com.yupi.springbootinit.model.dto.question;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * 创建题目请求
@@ -15,7 +16,7 @@ public class QuestionAddRequest implements Serializable {
     /**
      * 题目内容（json格式）
      */
-    private QuestionContentDTO questionContent;
+    private List<QuestionContentDTO> questionContent;
 
     /**
      * 应用 id
