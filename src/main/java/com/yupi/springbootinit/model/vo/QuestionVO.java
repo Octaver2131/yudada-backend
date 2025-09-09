7e68fa270b2cc211115bd575ec86135233e30ad6
Octaver <Octaver2131@outlook.com>
2025-09-09 12:15:53 +0800

    题目视图 修改

--- src/main/java/com/yupi/springbootinit/model/vo/QuestionVO.java
+++ src/main/java/com/yupi/springbootinit/model/vo/QuestionVO.java
@@ -1,6 +1,9 @@
 package com.yupi.springbootinit.model.vo;
 
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.yupi.springbootinit.model.dto.question.QuestionContentDTO;
 import com.yupi.springbootinit.model.entity.Question;
 import lombok.Data;
 import org.springframework.beans.BeanUtils;
@@ -24,14 +27,14 @@ public class QuestionVO implements Serializable {
     private Long id;
 
     /**
-     * 标题
+     * 题目内容（json格式）
      */
-    private String title;
+    private QuestionContentDTO questionContent;
 
     /**
-     * 内容
+     * 应用 id
      */
-    private String content;
+    private Long appId;
 
     /**
      * 创建用户 id
@@ -47,11 +50,6 @@ public class QuestionVO implements Serializable {
      * 更新时间
      */
     private Date updateTime;
-
-    /**
-     * 标签列表
-     */
-    private List<String> tagList;
 
     /**
      * 创建用户信息
@@ -70,8 +68,8 @@ public class QuestionVO implements Serializable {
         }
         Question question = new Question();
         BeanUtils.copyProperties(questionVO, question);
-        List<String> tagList = questionVO.getTagList();
-        question.setTags(JSONUtil.toJsonStr(tagList));
+        QuestionContentDTO questionContentDTO = questionVO.getQuestionContent();
+        question.setQuestionContent(JSONUtil.toJsonStr(questionContentDTO));
         return question;
     }
 
@@ -87,7 +85,7 @@ public class QuestionVO implements Serializable {
         }
         QuestionVO questionVO = new QuestionVO();
         BeanUtils.copyProperties(question, questionVO);
-        questionVO.setTagList(JSONUtil.toList(question.getTags(), String.class));
+        questionVO.setQuestionContent(JSONUtil.toBean(question.getQuestionContent(), QuestionContentDTO.class));
         return questionVO;
     }
 }
